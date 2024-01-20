edfebc8019e2556daa48798f8c6f72ac333ae2ab
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-20 14:56:05 +0530

    Update blocker.js

--- js/blocker.js
+++ js/blocker.js
@@ -1,56 +1,8 @@
-const defaultFilters = [
-	"*://*.doubleclick.net/*",
-	"*://*.doubleclick.com/*",
-	"*://partner.googleadservices.com/*",
-	"*://*.googlesyndication.com/*",
-	"*://*.google-analytics.com/*",
-	"*://creative.ak.fbcdn.net/*",
-	"*://*.adbrite.com/*",
-	"*://*.exponential.com/*",
-	"*://*.quantserve.com/*",
-	"*://*.scorecardresearch.com/*",
-	"*://*.zedo.com/*",
-	"*://*.googleadservices.com/*",
-	"*://*.omniture.com/*",
-	"*://*.intellitxt.com/*",
-	"*://*.2o7.net/*",
-	"*://*.207.net/*",
-	"*://*.sharethis.com/*",
-	"*://*.shopthetv.com/*",
-	"*://*.acoda.net/*",
-	"*://*.targetingmarketplace.com/*",
-	"*://*.youknowbest.com/*",
-	"*://*.adsense.com/*",
-	"*://*.adblade.com/*",
-	"*://*.247realmedia.com/*",
-	"*://*.2mdn.net/*",
-	"*://*.foxnetworks.com/*",
-	"*://*.gravity.com/*",
-	"*://*.hitbox.com/*",
-	"*://*.nielsen-online.com/*",
-	"*://*.quantcast.com/*",
-	"*://*.esomniture.com/*",
-	"*://*.atwola.com/*",
-	"*://*.channelintelligence.com/*",
-	"*://*.aquantive.com/*",
-	"*://*.addthis.com/*",
-	"*://*.afy11.net/*",
-	"*://*.ru4.com/*",
-	"*://*.samsungadhub.com/*",
-	"*://*.myads.com/*",
-	"*://*.rmxads.com/*",
-	"*://*.nexac.com/*",
-	"*://*.imrworldwide.com/*",
-	"*://*.leadback.com/*",
-	"*://*.offermatica.com/*",
-	"*://*.admob.com/*",
-	"*://*.adsonar.com/*",
-	
-
-]
-
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return { cancel: true }},
-    { urls: defaultFilters },
+    function(details) {
+        console.log("Block:", details.url)
+        return {cancel: true}
+    },
+    {urls: site},
     ["blocking"]
 )

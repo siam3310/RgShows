1f12b00a3ed5780727fe86fce23de78cb20ae83d
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-20 15:06:58 +0530

    Update blocker.js

--- js/blocker.js
+++ js/blocker.js
@@ -6,20 +6,3 @@ chrome.webRequest.onBeforeRequest.addListener(
     {urls: site},
     ["blocking"]
 )
-
-document.oncontextmenu = () => {
-    alert("Pls dont try to steal the codes Restricted By Rishab")
-    return false;
-
-}
-
-document.onkeydown = e => {
-    if(e.key == "F12"){
-        alert("Pls dont try to steal the codes Restricted By Rishab")
-        return false
-    }
-    if(e.ctrlKey && e.key == "u") {
-        alert("Pls dont try to steal the codes Restricted By Rishab")
-        return false
-    }
-}

ec77bc3d5763f324dc7a145100c699138890da9b
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-20 14:50:03 +0530

    Update blocker.js

--- js/blocker.js
+++ js/blocker.js
@@ -1,20 +1,26 @@
-const defaultFilters = [
-	"*://*.doubleclick.net/*",
-	"*://partner.googleadservices.com/*",
-	"*://*.googlesyndication.com/*",
-	"*://*.google-analytics.com/*",
-	"*://creative.ak.fbcdn.net/*",
-	"*://*.adbrite.com/*",
-	"*://*.exponential.com/*",
-	"*://*.quantserve.com/*",
-	"*://*.scorecardresearch.com/*",
-	"*://*.zedo.com/*",
+const adList = [
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googletagservices.com/*",
     "*://*.googleadservices.com/*",
-    "*://*.moat.com/*"
-]
+    "*://*.google-analytics.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.adbureau.net/*",
+    "*://*.carbonads.net/*",
+    "*://*.cdn.carbonads.com/*",
+    "*://*.cdn.carbonads.net/*",
+    "*://*.cdn.doubleclick.net/*",
+    "*://*.cdn.googletagservices.com/*",
+    "*://*.cdn.googleadservices.com/*",
+    "*://*.cdn.zedo.com/*",
+];
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return { cancel: true }},
-    { urls: defaultFilters },
+    function(details) {
+        return {cancel: true}
+    },
+    {urls: adList},
     ["blocking"]
 )

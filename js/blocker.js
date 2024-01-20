1bd2ad170655de4d9b7f5984218a66276d174535
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-20 14:47:53 +0530

    Update blocker.js

--- js/blocker.js
+++ js/blocker.js
@@ -1,37 +1,20 @@
-// Check for ads and click the skip button if available
-function checkForAds() {
-    const skipButton = document.querySelector(".ytp-ad-skip-button");
-    if (skipButton) {
-        console.log("Ad Detected. Skipping...");
-        skipButton.click();
-    }
-}
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.moat.com/*"
+]
 
-// Run the ad-checking function every second (1000 milliseconds)
-setInterval(checkForAds, 1000);
-
-
-// Function to hide the ad container with the id "player-ads"
-function hidePlayerAds() {
-    const playerAds = document.getElementById('player-ads');
-    if (playerAds) {
-        playerAds.style.display = 'none';
-    }
-}
-
-// Function to hide ad slots with the class "yt-ad-slot-renderer"
-function hideAdSlotRenderers() {
-    const adSlotRenderers = document.querySelectorAll('.yt-ad-slot-renderer');
-    adSlotRenderers.forEach((adSlotRenderer) => {
-        adSlotRenderer.style.display = 'none';
-    });
-}
-
-// Function to run both ad-blocking functions
-function blockAds() {
-    hidePlayerAds();
-    hideAdSlotRenderers();
-}
-
-// Run the blockAds function every second
-setInterval(blockAds, 1000);
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)

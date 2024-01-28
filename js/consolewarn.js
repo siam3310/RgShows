8906ac529f43c5418bcfac588166ab205214ea21
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-28 16:39:01 +0530

    Create consolewarn.js

---
+++ js/consolewarn.js
@@ -0,0 +1,2 @@
+console.log('%cStop!', 'color: red; font-size: 30px; font-weight: bold;');
+console.log('%cThis is a browser feature intended for developers. If someone told you to copy-paste something here, it is a scam and could potentially harm your computer.', 'font-size: 18px;');

00079f780f47461b77fc442e161d3ec8435afd8c
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-28 16:48:14 +0530

    Update consolewarn.js

--- js/consolewarn.js
+++ js/consolewarn.js
@@ -1,4 +1,6 @@
-console.log('%cStop! Damn Congrats you succeeded to open dev tabs 🎉', 'color: red; font-size: 30px; font-weight: bold;');
+console.log('%cStop!', 'color: red; font-size: 30px; font-weight: bold;');
+console.log('%cCongrats you succeeded to open dev tabs 🎉', 'color: red; font-size: 30px; font-weight: bold;');
 console.log('%cWell, you can join us too, join our discord https://discord.gg/bosskingdom-comeback-1090560322760347649 ✨', 'font-size: 18px;');
-console.log('%cSub my yt too  https://www.youtube.com/c/@ItzRishabBoss 😳 Here my github : https://github.com/RISHAB-CREATOR 👀', 'font-size: 18px;');
+console.log('%cSub my yt too  https://www.youtube.com/c/@ItzRishabBoss 😳', 'font-size: 18px;');
+console.log('%cHere my github : https://github.com/RISHAB-CREATOR 👀', 'font-size: 18px;');
 console.log('%cBye Dude, have fun with inspecting the codes, Happy Coding! 😎', 'font-size: 18px;');
